c3880205f07bcbaab068c92a05b3f9f81c5e27d3
DiaGreine <44259480+DiaGreine@users.noreply.github.com>
2020-02-09 20:07:46 +0700

    Add files via upload

---
+++ HW_1 Blinky/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+//HW_01
+
+DigitalOut led(LED1);
+DigitalIn button(PA_0);
+
+int main()
+{
+    while (true)
+    {
+        if(led.read() == 1 && button.read() == 1) {
+            led.write(0);
+        } else if (led.read() == 0 && button.read() == 1) {
+            thread_sleep_for(3000);
+            if(button.read() == 1) {
+                led.write(1);
+                while (true) {
+                    if (button.read() == 0)
+                        break;
+                }
+            }
+        }
+        
+    }
+}
