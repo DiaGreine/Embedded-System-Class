00087d609880bfc05a525b45c1c885a4565d4e32
Jirapat Suksiripornrit <jirapatsuksiripornrit@gmail.com>
2020-02-13 20:47:07 +0700

    no message

--- HW_2 TouceScreen/main.cpp
+++ HW_2 TouceScreen/main.cpp
@@ -4,6 +4,9 @@
 #include "stm32f413h_discovery_lcd.h"
 
 TS_StateTypeDef  TS_State = {0};
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
 
 int main()
 {
@@ -19,13 +22,9 @@ int main()
     /* Clear the LCD */
     BSP_LCD_Clear(LCD_COLOR_WHITE);
 
-    /* Set Touchscreen Demo1 description */
-    BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-    BSP_LCD_FillRect(0, 0, BSP_LCD_GetXSize(), 40);
-    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-    BSP_LCD_SetBackColor(LCD_COLOR_GREEN);
-    BSP_LCD_SetFont(&Font16);
-    BSP_LCD_DisplayStringAt(0, 15, (uint8_t *)"Touch the screen", CENTER_MODE);
+    int zonesize = BSP_LCD_GetXSize() / 3;
+    int line1 = zonesize;
+    int line2 = zonesize * 2;
 
     while (1) {
         BSP_TS_GetState(&TS_State);
@@ -35,12 +34,21 @@ int main()
             /* Get X and Y position of the first touch post calibrated */
             x1 = TS_State.touchX[0];
             y1 = TS_State.touchY[0];
-            printf("Touch Detected x=%d y=%d\n", x1, y1);
+        
+            if (0 <= x1 <= line1) {
+                led1.write(1);
+            } else if (line1 <= x1 <= line2) {
+                led2.write(1);
+            } else if (line2 <= x1 <= BSP_LCD_GetXSize()) {
+                led3.write(1);
+            }
 
-            BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-            BSP_LCD_FillCircle(x1, y1, 20);
 
             thread_sleep_for(10);
+        } else {
+            led1.write(0);
+            led2.write(0);
+            led3.write(0);
         }
     }
 }
